6c76ecc496b32f33f4260c6f39a8fe7e8417f99a
sszllx <sszllx@gmail.com>
2017-07-10 18:42:28 +0800

    update

--- ftpdownloader.sh
+++ ftpdownloader.sh
@@ -1,27 +1,36 @@
 #!/bin/bash
 
+destdir=./ioslogs
 ioslog=$(ncftpls -u ftpuser -p fa322Agwug ftp://123.59.134.139/ioslogs | sort | grep -v bak | tail -n 1)
-
 ioslog=$(echo | awk '{split("'${ioslog}'", array, "/");print array[2]}')
 
 echo $ioslog
 
-# ncftpget -u ftpuser -p fa322Agwug ftp://123.59.134.139/ioslogs/$ioslog
+ncftpget -u ftpuser -p fa322Agwug ftp://123.59.134.139/ioslogs/$ioslog
 
-cat $ioslog | awk '{print $1"\t"$2"\t"$3}' > test.id
+cat $ioslog | awk '{print $1"\t"$2"\t"$3}' > immediate.id
 
-linenum=$(wc -l $ioslog | awk '{print $1}' | xargs -i echo '{}/5' | bc)
-split -l $linenum $ioslog
+linenum=$(wc -l immediate.id | awk '{print $1}' | xargs -i echo '{}/5' | bc)
+split -l $linenum immediate.id
 
-md5val=$(md5sum $ioslog | awk '{print $1}')
+
+cat xaf >> xae
+rm xaf
+mv xaa $destdir/1
+mv xab $destdir/2
+mv xac $destdir/3
+mv xad $destdir/4
+mv xae $destdir/5
+
+md5val=$(md5sum immediate.id | awk '{print $1}')
 echo $md5val
 
-#bak_dir=$(echo | awk '{split("'${ioslog}'", array, ".");print array[5]}')
-#echo $bak_dir
+bak_dir=$(echo | awk '{split("'${ioslog}'", array, ".");print array[5]}')
+echo $bak_dir
 
-#mkdir $bak_dir
-#mv $ioslog $bak_dir
+mkdir $bak_dir
+mv $ioslog $bak_dir
 
-echo $md5val > md5
+echo $md5val > $destdir/md5
 
 echo "finish"
